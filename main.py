1aaf137487ce806665d78c9d1a991c8cb4c06554
garima125me-jpg <garima125me@gmail.com>
2026-04-24 01:35:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,8 +10,8 @@ import logging
 import sys
 # ╔══════════════════════════════════════════════════════════════════╗
 # ║  CREATOR: TARIKUL ISLAM
-# ║  TELEGRAN: https://t.me/paglu_dev
-# ║  PERSONAL TELEGRAM: https://t.me/itzpaglu
+# ║  TELEGRAN: https://t.me/primesahil1251012
+# ║  PERSONAL TELEGRAM: https://t.me/primesahil1251012 
 # ╚══════════════════════════════════════════════════════════════════╝
 
 # Configure logging
@@ -28,10 +28,10 @@ if not BOT_TOKEN:
     logger.error("❌ BOT_TOKEN not found! Please set your bot token in environment variables.")
     sys.exit(1)
 
-REQUIRED_CHANNELS = ["@your channel username"]
-GROUP_JOIN_LINK = "https://t.me/your_group_link"
-OWNER_ID = your tg user id (integer)    #Example: 6282811167
-OWNER_USERNAME = "@your username"
+REQUIRED_CHANNELS = ["@sahilxlike"]
+GROUP_JOIN_LINK = "https://t.me/sahilxlike"
+OWNER_ID = 7371842257
+OWNER_USERNAME = "@primesahil1251012"
 
 bot = telebot.TeleBot(BOT_TOKEN)
 like_tracker = {}   # in-memory cache
